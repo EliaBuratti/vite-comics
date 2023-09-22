9939d583f576d84b134d3ed8e07bf9718d5fe4f9
EliaBuratti <elia.buratti.dev@gmail.com>
2023-09-22 16:50:57 +0200

    create list template

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -7,9 +7,30 @@ export default {
 
 
 <template>
-    <div>
-        <h1 class="bg-secondary w-100">navBar</h1>
-    </div>
+    <h1 class="bg-secondary w-100">navBar</h1>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="../assets/img/dc-logo.png" alt="Logo" width="30" height="30"
+                    class="d-inline-block align-text-top">
+            </a>
+        </div>
+        <div class="eb_nav-section">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
 </template>
 
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './assets/scss/app.scss' //importo il css
 import App from './App.vue'
+import * as bootstrap from '../node_modules/bootstrap' //importo il js di bootstrap
 
 createApp(App).mount('#app')
