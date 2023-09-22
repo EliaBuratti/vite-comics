a61046f26a9911d18dfbb506e9d365f983856617
EliaBuratti <elia.buratti.dev@gmail.com>
2023-09-22 18:10:18 +0200

    add skeleton main content, not complete

--- src/components/mainContent.vue
+++ src/components/mainContent.vue
@@ -1,14 +1,38 @@
 <script>
 export default {
-    name: 'mainContent'
+    name: 'mainContent',
+
+    data() {
+
+        return {
+
+            cards: [
+                {
+                    text: 'digital comics',
+                    image: 'buy-comics-digital-comics.png', // persorso da sistemare!!!
+                },
+            ]
+        }
+    }
 }
 </script>
 
 
 
 <template>
-    <div>
-        <h1 class="bg-secondary w-100"> -- Content Goes here --</h1>
+    <div class="aboutUs">
+        <div class="container d-flex">
+            <!--             <div class="col">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="digital">
+                <span class="text-white text-uppercase">digital comics</span>
+            </div> -->
+
+            <div v-for="card in cards" class="col">
+                <img :src="card.image" alt="digital">
+                <span class="text-white text-uppercase">{{ card.text }}</span>
+            </div>
+
+        </div>
     </div>
 </template>
 
@@ -16,7 +40,7 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
-h1 {
-    color: $white-color;
+.aboutUs {
+    background-color: $primary-color;
 }
 </style>
